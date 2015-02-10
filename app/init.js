590b8723ef7501da44d4da2e1655419dec5bc35f
stephenjoe.a <stephenjoe.a@gmail.com>
2015-02-10 11:06:00 +0530

    unwanted code removed

--- app/init.js
+++ app/init.js
@@ -37,44 +37,6 @@ app.post('/newrespository', routes.newrespository);
 
 app.post('/deleterespository', routes.deleterespository); 
 
-/*
-//Get all posts
-app.get('/post/all', jwt({secret: secret.secretToken}), tokenManager.verifyToken, routes.posts.listAll);
-
-//Get the post id
-app.get('/post/:id', routes.posts.read); 
-
-//Like the post id
-app.post('/post/like', routes.posts.like);
-
-//Unlike the post id
-app.post('/post/unlike', routes.posts.unlike);
-
-//Get posts by tag
-app.get('/tag/:tagName', routes.posts.listByTag); 
-
-//Create a new user
-app.post('/user/register', routes.users.register); 
-
-//Login
-app.post('/user/signin', routes.users.signin); 
-
-//Logout
-app.get('/user/logout', jwt({secret: secret.secretToken}), routes.users.logout); 
-
-//Create a new post
-app.post('/post', jwt({secret: secret.secretToken}), tokenManager.verifyToken , routes.posts.create); 
-
-//Edit the post id
-app.put('/post', jwt({secret: secret.secretToken}), tokenManager.verifyToken, routes.posts.update); 
-
-//Delete the post id
-app.delete('/post/:id', jwt({secret: secret.secretToken}), tokenManager.verifyToken, routes.posts.delete); 
-
-//Serve the rss feed
-app.get('/rss', routes.rss.index);
-*/
-
 //client page
 
 app.get('/', function(req, res) {
